c3a4b20cb0c76c5947539ae468b77a31639b1a89
called2voyage <josie.stewart@bluesharkfriends.com>
2026-01-13 12:55:31 -0700

    A couple more fixes

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));
 
+const partialsMiddleware = (req, res, next) => {
+  app.set('views', __dirname + '/views');
+
+  next();
+};
+
 // use res.render to load up an ejs view file
 
 let branding_data = require('./views/data/_branding.json')
 
 // index page
 let index_data = require('./views/data/index.json')
-app.get('/', function(req, res) {
+app.get('/', partialsMiddleware, function(req, res) {
   res.render('pages/index', {index_data: index_data, branding_data: branding_data});
 });
 
@@ -19,3 +25,5 @@ app.get('/', function(req, res) {
 
 app.listen(9002);
 console.log('Server is listening on port 9002');
+
+module.exports = app;
